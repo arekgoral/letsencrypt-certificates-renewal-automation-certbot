316535157f338b36fcd7e0800ef90d66e3f6cada
arekgoral <arek.goral@txperfect.com>
2023-04-27 22:25:05 +0200

    first commit

---
+++ certcreator/createcert.py
@@ -0,0 +1,32 @@
+import boto3
+import certbot.main
+import os
+
+def create_certificate(domain_name, bucket_name, key_name):
+    # Create an S3 client
+    s3 = boto3.client('s3')
+
+     # Create a temp directory if it doesn't exist
+    temp_dir = 'temp'
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+
+    # Create a new certificate using Certbot
+    certbot.main.main([
+        '-v',
+        'certonly',
+        '--preferred-challenges', 'dns',
+        '--manual',
+        '--work-dir','temp',
+        '--config-dir', 'temp',
+        '--logs-dir', 'temp',
+        '-d', domain_name])
+
+    # Upload the certificate files to S3
+    with open(temp_dir + '/live/' + domain_name + '/privkey.pem', 'rb') as f:
+        s3.upload_fileobj(f, bucket_name, key_name + '/privkey.pem')
+    with open(temp_dir + '/live/' + domain_name + '/fullchain.pem', 'rb') as f:
+        s3.upload_fileobj(f, bucket_name, key_name + '/fullchain.pem')
+
+# Example usage
+create_certificate('testdomain.net', 'certificates_bucket', 'mycerts')
